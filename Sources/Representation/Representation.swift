17a6d4f7ab90a1b1c5d34ece78726c4a64798aa3
cherrywoods <cherrywoods@posteo.org>
2018-02-14 09:28:03 +0100

    Updated documentation

--- Sources/Representation/Representation.swift
+++ Sources/Representation/Representation.swift
@@ -13,11 +13,13 @@ import Foundation
  A protocol for any kind of encoded data, for example Data, or [String:Any].
  
  To use this protocol extend your raw data type to conform to this protocol.
- All you have to do then is implementing coder with a TranslatingCoder, that you will initalize with your implementation of Translator.
+ All you have to do then is implementing provideNewEncoder() and provideNewDecoder(). Just return a new MetaEncoder/MetaDecoder there.
  
  After extending your raw type, you can use the init(from: Decoder) and encode(to: Encoder) methods of your Decodable and Encodable swift types directly.
  
- It is usefull to write a subprotocol of Representation before you extend your raw data type. For example, if you want to write another library for pasing JSON, first write a protocol JSON: Representation, for which you provide the default implementation in an extension and then extends your raw type, e.g. Data by JSON.
+ It is usefull to write a subprotocol of Representation before you extend your raw data type.
+ For example, if you want to write another library for parsing JSON,
+ first write a protocol JSON: Representation, for which you provide the default implementation in an extension and then extends your raw type, e.g. Data by JSON.
  When you encode or decode to or from the raw type you should give the reader and swift a hint what format to use, by adding `as JSON` to the raw types object. This will save you time if you decide to add another serialization framwork to your application that has the same raw representation type and will give some hints about what you are doing right inside your code.
  */
 public typealias Representation = EncodingRepresentation & DecodingRepresentation
