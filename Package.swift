f005cd61207efd12837d72d9a8ae4190b1251fa9
Isaac Greenspan <ilg@2718.us>
2024-04-02 13:17:32 -0700

    Add test target to Package.swift.
    Also, symlink Examples and MoreTests inside Tests so that they are picked up by the test target.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0.2
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,10 +8,23 @@ let package = Package(
     products: [
         .library(name: "MetaSerialization", targets: ["MetaSerialization"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.5"),
+    ],
     targets: [
         .target(
             name: "MetaSerialization",
             path: "Sources"
+        ),
+        .testTarget(
+            name: "MetaSerializationTests",
+            dependencies: [
+                .byName(name: "Quick"),
+                .byName(name: "Nimble"),
+                .target(name: "MetaSerialization"),
+            ], 
+            path: "Tests"
         ),
     ]
 )
